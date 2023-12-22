96efb10fb56042f42643be3c75306f15e69452f1
root <root@3X4J8S3.nco.inet>
2023-12-22 14:46:33 +0100

    Quartz sync: Dec 22, 2023, 2:46 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,9 +6,6 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
 }
